0afe3e7f8f79a055796739f1853be6a6e06b5e71
cschweda <cschweda@gmail.com>
2017-06-06 15:11:48 -0500

    Update site with PDF files and summit documents.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,13 @@
 
 
 
-             <a style="margin-top: 25px; font-size:12px; font-weight: 700; text-transform: uppercase;" href="#" class="btn btn-primary">Presentations, Power Points & Resources</a>
+             <a style="margin-top: 25px; font-size:12px; font-weight: 700; text-transform: uppercase;" href="#" class="btn btn-primary" v-scroll-to="{
+     el: '#section-2',
+
+     duration: 800,
+     easing: 'linear',
+     offset: 20
+ }">Presentations, Power Points & Resources</a>
 
              <!-- <button class="btn btn-primary" v-scroll-to="'#section-2'" style="margin-top: 25px;">Register Now!</button> -->
 
@@ -90,13 +96,12 @@
 
 <section id="section-2">
 <div class="container">
-  <div class="row">
-    <div class="col-md-6">
 
+<div class="row">
+  <div class="col-md-12">
+    <div class="text-center">
       <h2 class="col-head">Presentations, Power Points & Resources</h2>
-
-
-
+    </div>
       <p>The 2017 ARI All-Sites Summit will offer peer
         learning around innovations at ARI sites,
         community reintegration, and partnerships.
@@ -104,37 +109,145 @@
         in local ARI programs, veterans courts, reducing employment barriers through criminal records relief, restorative justice, and more.
 </p>
 
+<div class="text-center" style="margin-bottom: 50px">
+  <a href="/static/summit_documents/Agenda_ARI_AllSites_Summit _2017_FINAL.pdf" class="btn btn-primary">Download Summit agenda&nbsp;&nbsp;<i class="fa fa-file-pdf-o" aria-hidden="true"></i></a></div>
+    </div>
+  </div>
+
+
+
+  <div class="row">
+    <div class="col-md-6">
+
+
+
+
+
+
+
 <!-- <p>For more information, contact <a href="lindsey.lapointe@illinois.gov">lindsey.lapointe@illinois.gov</a>,
 Program Manager, <a href="http://www.icjia.state.il.us/redeploy">Adult Redeploy Illinois</a>.</p> -->
 
 
-<div>
+<div id="resources">
+
+  <div class="card">
+    <h3 class="card-header">Prison and Sentencing Reform: ARI as a Vehicle for Justice System Improvement</h3>
+    <div class="card-block">
+
+      <ul>
+      <li>Dave Olson, Ph.D. and Don Stemen, Ph.D., Center for Criminal Justice Research, Policy and Practice, Loyola University of Chicago</li>
+      </ul>
+      <p class="card-text">This presentation provides an overview of trends in prison utilization in Illinois, what is driving these trends, how and why prison utilization varies across Illinois’ counties, and the opportunities and challenges to achieving criminal justice and sentencing reform in Illinois. Included will be a discussion of specific recommendations from the Illinois Governor’s Commission on Criminal Justice and Sentencing Reform that have implications for local criminal justice practice and policy, and how the ARI model can be seen as a vehicle for achieving the goals of reduced reliance on incarceration and improved public safety in Illinois.</p>
+
+      <ul>
+      <li><a href="/static/summit_documents/Loyola_Prison_and_sentencing_reform_ARI_PPT_Summit2017.pdf">Power Point</a></li>
+      <li><a href="/static/summit_documents/Loyola_ARI_advancing_justice_system_effectiveness_Summit2017.pdf">Document</a></li>
+      </ul>
+
+    </div>
+  </div>
 
 
+  <div class="card">
+  <h3 class="card-header">Bridges to Justice: A Community Engagement Toolkit for Adult Diversion Programs – Introduction of a New Resource for ARI Sites</h3>
+  <div class="card-block">
+    <ul>
+    <li>Angelique Orr, M.S., ARI Oversight Board Member</li>
+    <li>Amanda Hwu, former ARI staff</li>
+    <li>Lindsey LaPointe, ARI Program Manager</li>
+    </ul>
+    <p class="card-text">This presentation will roll out a new toolkit that provides a roadmap to strengthen the role of community in local ARI programs. The toolkit’s authors will describe the process of developing the toolkit and share key findings. Snapshots of ARI sites within the toolkit highlight robust community involvement and include program descriptions, challenges and navigation strategies related to community involvement, and how community involvement has enhanced programs. </p>
 
+    <ul>
+    <li><a href="/static/summit_documents/Community_toolkit_PPT_Summit2017.pdf">Power Point</a></li>
+    <li><a href="/static/summit_documents/Summary_bridges_to_justice_Summit2017.pdf">Document 1</a></li>
+    <li><a href="/static/summit_documents/Community_toolkit_bridges_to_justice_FINAL.pdf">Document 2</a></li>
+    </ul>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  </div>
 </div>
 
+
+<div class="card">
+  <h3 class="card-header">Restorative Justice in Action:  Not just for Juveniles</h3>
+  <div class="card-block">
+    <ul>
+    <li>Kathryn Rayford, B.S., Director of Training, Illinois Balanced and Restorative Justice </li>
+    <li>Gregory A. Mattingley, Macon County Board Member and Community Restorative Board volunteer</li>
+    </ul>
+
+    <p class="card-text">This session will provide an overview of Restorative Justice and how the various practices can be applied in adult situations. The presenters will discuss the stakeholders and how accountability, combined with competency development, contributes to community safety. The challenges and successes of implementing a Community Restorative Board in Macon County will be highlighted.  </p>
+
+    <ul>
+    <li><a href="/static/summit_documents/IBARJ_Restorative_Justice_in_action_PPT_Summit2017.pdf">Power Point</a></li>
+    </ul>
+  </div>
+</div>
+
+
+
+
+<div class="card">
+  <h3 class="card-header">Veterans Courts and Highlights of Veteran Mentors Program</h3>
+  <div class="card-block">
+    <ul>
+    <li>Judge Annette Eckert (Ret.)</li>
+    <li>Kelly Gallivan-Ilarraza, Problem-Solving Court Coordinator, Administrative Office of Illinois Courts</li>
+    <li>Michelle Rock, JD, Director, Illinois Center of Excellence for Behavioral Health and Justice</li>
+    <li>Rudy Martin, Lake County Veterans Court </li>
+    </ul>
+
+    <p>This session covers the importance and need for veterans courts; the current landscape of veterans        courts in Illinois, including current legislation and standards; the importance of planning for veterans courts; implementation; rural issues; and a highlight of innovations from the Lake County Veterans Treatment and Assistance Court.</p>
+
+    <ul>
+    <li><a href="/static/summit_documents/Veterans_courts_and_vet_mentoring_PPT_Summit2017.pdf">Power Point</a></li>
+    <li><a href="/static/summit_documents/Lake_County_VTAC_mentoring_communitytoolkit.pdf">Document</a></li>
+    </ul>
+  </div>
+</div>
+
+
+<div class="card">
+  <h3 class="card-header">Gender Responsive Justice Programming</h3>
+  <div class="card-block">
+    <ul>
+    <li>Deanne Benos, Founder &amp; Project Director, The Women’s Justice Initiative </li>
+    <li>Alyssa Benedict, Executive Director, CORE Associates</li>
+    <li>Margaret Burke, Warden, Logan Correctional Center</li>
+    <li>Carolyn Gurski, Chief of Public Safety, Illinois Department of Corrections</li>
+    </ul>
+
+    <p class="card-text">This presentation will share the Chicago-based Women’s Justice Initiative’s “Call to Action” that has begun in partnership with the Illinois Department of Corrections (IDOC) and is rooted in the most comprehensive Gender Informed Practices Assessment (GIPA) and Reform Strategy ever conducted by a state system. Panelists will invite participants to become part of a gender responsive justice systems change movement, will be provided with an overview of gender responsive tools available to them to improve outcomes among justice-involved women and girls, and will be offered the opportunity to share in a dialogue about frontline implementation challenges and solutions focused on overcoming longstanding culture differences and perceptions of impact vs accountability. </p>
+
+  </div>
+</div>
+
+
+<div class="card">
+  <h3 class="card-header">Improving and Sustaining Quality over the Life Cycle of Programs</h3>
+  <div class="card-block">
+    <ul>
+    <li>Lily Gleicher, M.S., ABD, Research Analyst, Illinois Criminal Justice Information Authority   </li>
+    </ul>
+
+    <p>This presentation provides a brief overview of evidence-based practices (EBPs) and logic models, with a focus on implementation science. Implementation is defined as the set of specified, purposeful, and detailed processes designed to put an innovation into practice. Thus, implementation science is defined as, “the factors that influence the full and effective use of innovations in practice” (National Implementation Research Network, 2015). In particular, this presentation will provide information regarding the importance of implementation fidelity, functional stages of implementation, core components of implementation, common implementation barriers, and program adaptation/modification. </p>
+
+    <ul>
+    <li><a href="/static/summit_documents/Improving_sustaining_program_quality_PPT_2017Summit.pdf">Power Point</a></li>
+    </ul>
+
+
+  </div>
+</div>
+
+</div>
 
     </div>
 
     <div class="col-md-6">
 
-      <h2 class="col-head">Registration, Lodging & Meals</h2>
+      <!-- <h2 class="col-head">Registration, Lodging & Meals</h2>
 
 
       <p class="text-center">
@@ -145,14 +258,10 @@ Program Manager, <a href="http://www.icjia.state.il.us/redeploy">Adult Redeploy 
 Bloomington, IL 61704 <br/>
 May 17 and 18, 2017<br/>
 <div style="margin-top: 15px;">
-<!-- <a href="http://www.bloomingtonchateau.com/" class="">
-  Hotel information&nbsp;&raquo;</a> -->
+
   <br/>
 
-<!-- https://www.surveygizmo.com/s3/3433332/2017-Adult-Redeploy-Illinois-All-Sites-Summit-Registration -->
-<!-- <button class="btn btn-secondary btn-register spacerTop30" disabled>
- REGISTRATION OPEN 3/29/17 TO 4/12/17
- </button> -->
+
 
 
 </div>
@@ -181,7 +290,118 @@ May 17 and 18, 2017<br/>
 <li>Expected start time is 8:30am on both days, with coffee and check-in beginning at 7:45am. </li>
 <li>Morning coffee and light pastries will be provided.</li>
 <li>The Summit will conclude about 4:30pm on Thursday, May 18th.</li>
-</ul>
+</ul> -->
+
+
+
+
+
+<div class="card">
+  <h3 class="card-header">ARI Site &amp; Partner Workshop: Using Logic Models and Data to Inform Program Oversight and Management</h3>
+  <div class="card-block">
+    <ul>
+    <li>Lynne Mock, Ph.D, ARI Research Manager, ICJIA</li>
+    <li>Lily Gleicher, M.S., ABD, Research Analyst, ICJIA</li>
+    <li>Megan Alderden, Ph.D, Associate Director of Research, ICJIA</li>
+    </ul>
+
+    <p>This session begins a dialogue between ARI site representatives and researchers about the processes and outcomes of their programs. Rather than focus strictly on an outcome evaluation, the focus will be on how client outcomes occur and sites will learn the usefulness of their data for program management. Researchers will share a basic structure underlying the development of performance measurement for programs and ask for feedback on the structure’s validity. The feedback will be used to review the logic model and program evaluation plans, in addition to the development of a quarterly information feedback process that program staff can use to review their progress, maintain good processes and outcomes, and strengthen their programs. </p>
+
+    <ul>
+    <li><a href="/static/summit_documents/Logic_models_and_data_oversight_management _PPT_2017Summit.pdf">Power Point</a></li>
+    <li><a href="/static/summit_documents/Logic_model_ISPS_handout.pdf">Document 1</a></li>
+    <li><a href="/static/summit_documents/Logic_model_drug_court_handout.pdf">Document 2</a></li>
+    <li><a href="/static/summit_documents/Logic_model_mental_health_court_handout.pdf">Document 3</a></li>
+    </ul>
+  </div>
+</div>
+
+
+<div class="card">
+  <h3 class="card-header">Navigating Criminal Records Relief in Illinois</h3>
+  <div class="card-block">
+    <ul>
+    <li>Esther Franco-Payne, Executive Director, Cabrini Green Legal Aid (CGLA)</li>
+    <li>Beth Johnson, Director of Legal Programs, CGLA</li>
+    <li>Cynthia Cornelius, Assistant Director of Legal Programs, CGLA</li>
+    <li>Katie Blakeman, Clerk of the Circuit Court of Champaign County </li>
+    </ul>
+
+    <p>This presentation provides ARI sites and stakeholders an overview of ways an individual can remove the barriers imposed by arrest and conviction records. As a complement to ARI’s goal of providing alternatives to incarceration and reducing crime in the State of Illinois, having knowledge of ways to navigate criminal records relief enables ARI staff to assist their clients in removing obstacles to stable housing, meaningful employment and educational opportunities. The presentation will not only support the individual clients engaged in programs, but aid ARI sites in achieving performance measurement indicators related to employment rates and recidivism.</p>
+
+    <ul>
+    <li><a href="/static/summit_documents/CGLA _Records_relief_PPT_2017Summit.pdf">Power Point 1</a></li>
+    <li><a href="/static/summit_documents/Champaign_PPT_2017Summit.pdf">Power Point 2</a></li>
+    <li><a href="/static/summit_documents/CGLA_certificates_of_relief_occupations_2017.pdf">Document 1</a></li>
+    <li><a href="/static/summit_documents/CLGA_recordsrelief_quickchart_2017.pdf">Document 2</a></li>
+    <li><a href="/static/summit_documents/CGLA_basic_expunge_seal_2017.pdf">Document 3</a></li>
+    <li><a href="/static/summit_documents/CLGA_flowchart_expunge_seal_2017.pdf">Document 4</a></li>
+    <li><a href="/static/summit_documents/CGLA_changed_employment_laws_2016_2017.pdf">Document 5</a></li>
+    <li><a href="/static/summit_documents/CGLA_dashboard_2017.pdf">Document 6</a></li>
+    <li><a href="/static/summit_documents/CGLA_true_false_quiz_2017.pdf">Document 7</a></li>
+    </ul>
+
+  </div>
+</div>
+
+
+<div class="card">
+  <h3 class="card-header">Policy and Public Opinion: The Impact of ARI on Criminal Justice Reform in Illinois</h3>
+  <div class="card-block">
+    <ul>
+    <li>Moderator: Brian Mackey, Statehouse Reporter, NPR Illinois <br>
+    Judge Elizabeth Robb (Ret.)</li>
+    <li>Joseph P. Bruscato, Winnebago County State’s Attorney </li>
+    <li>Tonya Veenstra, Will County ARI Program Graduate, Moraine Valley Community College Student</li>
+    </ul>
+
+    <p>This conversation will focus on criminal justice reform in Illinois, including current measures of public opinion on the issue and the ways that ARI has a policy voice (including around expansion of eligibility for diversion programs). The panelists will offer their perspectives and the audience will be invited to share their local experiences. </p>
+
+    <ul>
+    <li><a href="/static/summit_documents/ASJ_Illinois_crime_survivors_fullreport_2016.pdf">Document 1</a></li>
+    <li><a href="/static/summit_documents/Illinois_commission_CJ_sentencing_reform_final_report_Dec_2016.pdf">Document 2</a></li>
+    <li><a href="/static/summit_documents/IPI_CJ_reform_support_2016_full report.pdf">Document 3</a></li>
+    <li><a href="/static/summit_documents/ARI_eligibility_housebill_3905_May_2017.pdf">Document 4</a> </li>
+    <li><a href="/static/summit_documents/ACLU_poll_2016.pdf">Document 5</a></li>
+    </ul>
+  </div>
+</div>
+
+
+
+<div class="card">
+  <h3 class="card-header">ARI Site Workshop:  ARI Grants, Budget and the Grants Accountability and Transparency Act</h3>
+  <div class="card-block">
+    <ul>
+    <li>ARI and ICJIA Staff</li>
+    </ul>
+
+    <p>This session will cover a variety of topics to help grantees better understand the ICJIA grant review process. ICJIA team members will provide a breakdown of ARI-related roles and responsibilities. Other topics discussed that impact the grant review process include the State budget, GATA, Risk Assessments, Indirect Costs, Cost Allowability, Subcontracting, and Timekeeping documentation. Presenters include staff from ICJIA’s ARI, Legal, and Grant units. </p>
+
+    <ul>
+    <li><a href="/static/summit_documents/ICJIA_grants_budget_GATA_PPT_2017Summit.pdf">Power Point</a></li>
+    <li><a href="/static/summit_documents/Quarterly_time_keeping_certification.docx">Document 1</a></li>
+    <li><a href="/static/summit_documents/ICJIA_grantee_time_certification.docx">Document 2</a></li>
+    <li><a href="/static/summit_documents/ICJIA_timekeeping_provision.docx">Document 3</a></li>
+    <li><a href="/static/summit_documents/ICJIA_grantee_personnel_activity_report_template.xlsx">Document 4</a></li>
+    </ul>
+  </div>
+</div>
+
+
+
+<div class="card">
+  <h3 class="card-header">Sites Updates</h3>
+  <div class="card-block">
+    <ul>
+    <li><p>ARI sites and ICJIA Staff</p></li>
+    <li><p><a href="/static/summit_documents/ARI_site_descriptions_SFY17.pdf">Document</a></p></li>
+    </ul>
+  </div>
+</div>
+
+
+
 
 
 
@@ -191,7 +411,7 @@ May 17 and 18, 2017<br/>
 
 
   </div>
-<div class="row">
+<!-- <div class="row">
   <div class="col-md-12">
     <div class="text-center spacerTop50">
       <p>For more information, contact <a href="lindsey.lapointe@illinois.gov">lindsey.lapointe@illinois.gov</a>,
@@ -200,7 +420,7 @@ May 17 and 18, 2017<br/>
 
   </div>
 
-</div>
+</div> -->
 </div>
 
 </section>
@@ -270,4 +490,10 @@ May 17 and 18, 2017<br/>
 .col-head {padding-bottom: 10px; border-bottom: 1px solid #ccc; margin-bottom: 20px}
 .coming-soon {margin-top: 50px;}
 .hotel-info {margin-top: 50px; margin-bottom: 50px;}
+
+/*h2.resource-sub {border-bottom: 1px solid #aaa; padding-bottom: 10px; padding-top: 40px; line-height: 30px;}*/
+.card {margin-bottom: 30px;}
+
+.card-block a {text-decoration: underline}
+
 </style>
